2dfa5c6033f9604a28cf4d3226da7efcbacfaf3f
Pranav Kumar Verma <39586967+kumar-pranaw@users.noreply.github.com>
2021-10-25 12:54:05 -0700

    test: auto save

--- DatingApp/DatingAppServices/IDatingAppServices/IAuthService.cs
+++ DatingApp/DatingAppServices/IDatingAppServices/IAuthService.cs
@@ -6,5 +6,6 @@ namespace DatingAppServices.IDatingAppServices
     public interface IAuthService
     {
         Task<UserModel> Register(UserModel user);
+        Task TestForVSCode(UserModel user);
     }
 }
